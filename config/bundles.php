0e7f4f0a4f999f499e80c7280d9e9fe3af428e68
jordan-nguyen <dizorder3@gmail.com>
2022-10-26 20:32:07 +0200

    changes

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Nat\DeployBundle\NatDeployBundle::class => ['all' => true],
 ];
